3866d022e4a64fbde4e7f6527ae97409232c02c6
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-01-22 08:57:40 +0530

    #Day21

--- Palindrome Integer.cpp
+++ Palindrome Integer.cpp
@@ -1,1 +1,12 @@
-
+int Solution::isPalindrome(int A) {
+  if(A<0)  return 0;
+  int rev=0;
+  int temp=A;
+  while(temp){
+    int rem = A%10;
+    rev = rev*10+rem;
+    temp/=10;
+  }
+  if(A==rev)  return 1;
+  else return 0;
+}
