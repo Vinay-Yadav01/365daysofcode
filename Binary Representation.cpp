7b4ee026a1105ef12395576b4fa5cbf421f4a4ff
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-01-24 18:09:26 +0530

    #Day22

--- Binary Representation.cpp
+++ Binary Representation.cpp
@@ -1,1 +1,11 @@
-
+string Solution::findDigitsInBinary(int A) {
+    string ans = "";
+    if(A==0) return "0";
+    while(A){
+        int bit = A%2;
+        ans+=to_string(bit);
+        A/=2;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
