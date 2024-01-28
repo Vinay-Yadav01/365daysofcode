eebd753cde0e7e4efa3e794c7cf2dedadfecafd0
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-01-28 18:12:19 +0530

    #Day26

--- Excel Column Title.cpp
+++ Excel Column Title.cpp
@@ -1,1 +1,11 @@
-
+string Solution::convertToTitle(int A) {
+    string ans = "";
+    while(A){
+        char element = A%26+64;
+        if(A%26==0) ans+='Z';
+        else ans.push_back(element);
+        A=(A-1)/26;
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
