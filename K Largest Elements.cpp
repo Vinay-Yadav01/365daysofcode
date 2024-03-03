b3e405775f8eed4a602ac161c5247a5810f5e3f0
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-03 13:12:58 +0530

    #Day58

--- K Largest Elements.cpp
+++ K Largest Elements.cpp
@@ -1,1 +1,12 @@
-
+vector<int> Solution::solve(vector<int> &A, int B) {
+    vector<int> ans;
+    priority_queue<int> q;
+    for(auto i:A){
+        q.push(i);
+    }
+    while(B--){
+        ans.push_back(q.top());
+        q.pop();
+    }
+    return ans;
+}
