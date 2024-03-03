14f8957ed91878098e53c6bcea5e8a1b6458eff4
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-03 13:08:08 +0530

    #Day62

--- Smaller or equal elements.cpp
+++ Smaller or equal elements.cpp
@@ -6,23 +6,13 @@ int Solution::solve(vector<int> &A, int B) {
     while(low<=high){
         int mid=(low+high)/2;
         if(B==A[mid]){
-
             res=mid+1;
-
             low=mid+1;}
-
         else if(B<A[mid]){
-
             high =mid-1;
-
             res=mid;}
-
         else {low=mid+1;
-
             res=mid+1;}
-
     }
-
     return res;
-
 }
