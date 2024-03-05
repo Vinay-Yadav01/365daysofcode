4d289592bc9007b1ee1a035cdd812e57d6dded9e
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-05 19:15:56 +0530

    #Day63

--- Rotate List.cpp
+++ Rotate List.cpp
@@ -1,1 +1,24 @@
-Rotate List
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::rotateRight(ListNode* A, int B) {
+    ListNode* curr=A;
+    int count=1;
+    while(curr->next && ++count){
+        curr=curr->next;
+    }
+    curr->next=A;
+    B%=count;
+    B=count-B;
+    while(B--){
+        curr = curr->next;
+    }
+    A=curr->next;
+    curr->next=NULL;
+    return A;
+}
