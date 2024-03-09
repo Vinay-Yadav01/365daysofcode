a0cb86860ab84ce1f5e6562e517ae656a1a035e5
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-09 10:40:26 +0530

    #Day68

---
+++ Sort array with squares!.cpp
@@ -0,0 +1,13 @@
+vector<int> Solution::solve(vector<int> &A) {
+    priority_queue<int,vector<int>,greater<int>>minHeap;
+    for(int i=0;i<A.size();i++){
+        A[i]=A[i]*A[i];
+        minHeap.push(A[i]);
+    }
+    int index=0;
+    while(!minHeap.empty()){
+        A[index++]=minHeap.top();
+        minHeap.pop();
+    }
+    return A;
+}
