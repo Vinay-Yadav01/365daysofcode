a1761170fe6cf91f468823291cf17cd711203030
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-16 12:22:53 +0530

    #Day74

--- Positive Negative.cpp
+++ Positive Negative.cpp
@@ -1,1 +1,11 @@
-Positive Negative
+vector<int> Solution::solve(vector<int> &A) {
+    int pos=0,neg=0;
+    for(auto i:A){
+        if(i>0) pos++;
+        else if(i<0) neg++;
+    }
+    vector<int> ans;
+    ans.push_back(pos);
+    ans.push_back(neg);
+    return ans;
+}
