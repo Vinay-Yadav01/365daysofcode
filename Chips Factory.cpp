2a848446550023ec7cd17cf573110f69ce86dca5
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-16 12:21:04 +0530

    #Day72

--- Chips Factory.cpp
+++ Chips Factory.cpp
@@ -1,1 +1,11 @@
+vector<int> Solution::solve(vector<int> &A) {
+    vector<int> ans;
+    for(int i=0;i<A.size();i++){
+        if(A[i]>0) ans.push_back(A[i]);
+    }
+    while(ans.size()<A.size()){
+        ans.push_back(0);
+    }
+    return ans;
+}
 
