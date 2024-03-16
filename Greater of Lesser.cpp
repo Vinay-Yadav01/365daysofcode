4a8dda7819e41f084de479aacd201bfab26aaa44
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-16 12:22:06 +0530

    #Day73

--- Greater of Lesser.cpp
+++ Greater of Lesser.cpp
@@ -1,1 +1,11 @@
+int Solution::solve(vector<int> &A, vector<int> &B, int C) {
+    int maxi=0,mini=0;
+    for(auto i:A){
+        if(i>C) maxi++;
+    }
+    for(int i:B){
+        if(i<C) mini++;
+    }
+    return max(mini,maxi);
+}
 
