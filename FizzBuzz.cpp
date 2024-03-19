06b78a46329034a34615681f342c6a62d849913d
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-19 11:29:47 +0530

    #Day76

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,1 +1,13 @@
+vector<string> Solution::fizzBuzz(int A) {
+    vector<string> ans;
+    int i=1;
+    while(i<=A){
+        if(i%3==0 && i%5==0) ans.push_back("FizzBuzz");
+        else if(i%3==0) ans.push_back("Fizz");
+        else if(i%5==0) ans.push_back("Buzz");
+        else ans.push_back(to_string(i));
+        i++;
+    }
+    return ans;
+}
 
