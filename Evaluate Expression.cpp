29209542f732c6f26b76ee09f146d5f5e77f8635
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-19 11:30:44 +0530

    #Day77

--- Evaluate Expression.cpp
+++ Evaluate Expression.cpp
@@ -1,1 +1,20 @@
+int Solution::evalRPN(vector<string> &A) {
+    stack<int> s;
+    for(auto i:A){
+        if(i!="+" && i!="-" && i!="*" && i!="/"){
+            s.push(stoi(i));
+        }
+        else {
+            int a=s.top();
+            s.pop();
+            int b=s.top();
+            s.pop();
+            if(i=="+") s.push(a+b);
+            else if(i=="-") s.push(b-a);
+            else if(i=="*") s.push(a*b);
+            else s.push(b/a);
+        }
+    }
+    return s.top();
+}
 
