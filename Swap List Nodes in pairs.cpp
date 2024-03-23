4068816049f1aab870085f2973b83f24ddf84010
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-03-23 12:31:21 +0530

    #Day82

---
+++ Swap List Nodes in pairs.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::swapPairs(ListNode* A) {
+    ListNode* slow = A;
+    ListNode* fast=A;
+    if(A->next ==NULL) return A;
+    while(slow!= NULL && slow->next != NULL ){
+            fast=slow->next;
+            int temp=slow->val;
+            slow->val=fast->val;
+            fast->val=temp;
+            slow=slow->next->next;
+        
+    }
+    return A;
+}
