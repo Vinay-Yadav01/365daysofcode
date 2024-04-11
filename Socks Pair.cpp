19194e13aabd42868a862c6989135a8cd8cf4520
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-04-11 17:15:05 +0530

    #Day101

---
+++ Socks Pair.cpp
@@ -0,0 +1,11 @@
+int Solution::solve(vector<int> &A) {
+    unordered_map<int,int> mp;
+    for(int i=0;i<A.size();i++){
+        mp[A[i]]++;
+    }
+    int count=0;
+    for(auto x:mp){
+     count=count+x.second/2;
+    }
+    return count;
+}
