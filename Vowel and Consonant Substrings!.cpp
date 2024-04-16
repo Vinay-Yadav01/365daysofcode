04dc9774b62c65d4a859572102dfb2fab02b5c92
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-04-16 21:06:27 +0530

    #Day105

--- Vowel and Consonant Substrings!.cpp
+++ Vowel and Consonant Substrings!.cpp
@@ -1,1 +1,10 @@
+int Solution::solve(string A) {
+    const int mod = 1e9+7;
+    long long vowel =0, conso=0;
+    for(auto i:A){
+        if(i=='a' || i=='e' || i=='i' || i=='o' || i=='u') vowel++;
+        else conso++;
+    }
+    return (vowel*conso)%mod;
+}
 
