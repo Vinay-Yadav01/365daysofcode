e2472e9742c2dd47aa4a29fd61a7cf9c23390caf
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-04-20 11:40:51 +0530

    #Day110

--- Prime Sum.cpp
+++ Prime Sum.cpp
@@ -3,16 +3,12 @@ vector<int> sieve(int n) {
     vector<int>ans;   
     prime[0] = false;
     prime[1] = false;    
-    for(int i=2;i*i<=n;i++){
-        if(prime[i]){
-            for(int j=i*i;j<=n;j+=i){
-                prime[j] = false;
-            }
-        }
-    }
     for(int i=2;i<=n;i++){
         if(prime[i]){
             ans.push_back(i);
+            for(int j=i*2;j<=n;j+=i){
+                prime[j] = false;
+            }
         }
     }
     return ans;
