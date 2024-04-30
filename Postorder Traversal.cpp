78e4813f2471a28efb2645020a76d00976056a52
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-04-30 19:14:42 +0530

    #Day119

--- Postorder Traversal.cpp
+++ Postorder Traversal.cpp
@@ -1,1 +1,23 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+ void f(TreeNode* A, vector<int> &ans){
+     if(A==NULL) return;
+     f(A->left,ans);
+     f(A->right,ans);
+     ans.push_back(A->val);
+ }
+ 
+vector<int> Solution::postorderTraversal(TreeNode* A) {
+    vector<int> ans;
+    f(A,ans);
+    return ans;
+}
 
