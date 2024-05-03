73bd18cb67d46ecf588698fb41d40c75449fd3ad
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-05-03 17:34:30 +0530

    #Day123

---
+++ Path Sum.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+int flag=0;
+void sum(TreeNode* root, int B){
+    if(root==NULL) return;
+    if(B-root->val==0 && root->left==NULL && root->right==NULL){
+        flag=1;
+        return;
+    }
+    sum(root->left,B-root->val);
+    sum(root->right,B-root->val);
+}
+ 
+int Solution::hasPathSum(TreeNode* A, int B) {
+    flag=0;
+    sum(A,B);
+    return flag;
+}
