3a6fbae8e648173d675f440cb8e36a099d8a0164
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-05-16 19:31:48 +0530

    #Day136

---
+++ Balanced Binary Tree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+int height(TreeNode* A){
+
+        if(A==NULL){ return 0;}
+    int lh = height(A->left);
+    if(lh==-1){return -1;}
+    int rh = height(A->right);
+    if(rh==-1){return -1;}
+    if(abs(lh-rh)>1){ return -1;}
+    else{
+        return max(lh,rh)+1;
+    }
+}
+
+int Solution::isBalanced(TreeNode* A) {
+    if(height(A)==-1){ return 0;}
+    else {
+        return 1;
+    }
+}
