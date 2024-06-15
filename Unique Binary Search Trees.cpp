78d71cb287c5e48e81886d6e2e95b221b309a76d
Vinay Yadav <122151570+Vinay-Yadav01@users.noreply.github.com>
2024-06-15 18:13:21 +0530

    #Day166

---
+++ Unique Binary Search Trees.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ vector<TreeNode*> solve(int s,int e){
+     vector<TreeNode*> ans;
+     if(s>e) return {NULL};     
+
+     for(int i=s;i<=e;i++){
+         vector<TreeNode*> leftsubtree=solve(s,i-1);
+         vector<TreeNode*> rightsubtree=solve(i+1,e);
+         for(auto l:leftsubtree){
+             for(auto r:rightsubtree){
+                 TreeNode* newroot=new TreeNode(i);
+                 newroot->left=l;
+                 newroot->right=r;  
+                 ans.push_back(newroot);
+             }
+         }
+     }
+     return ans;
+ }
+
+vector<TreeNode*> Solution::generateTrees(int A) {
+    return solve(1,A);
+}
